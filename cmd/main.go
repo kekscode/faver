1b3c56402f6481e94c08f0a5b660be163865fe59
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-07-23 13:08:11 +0200

    Add (naive) concurrency to speed things up

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/kekscode/faver/internal"
@@ -29,49 +30,55 @@ func main() {
 		fmt.Println("Reading positional arguments")
 		targets = flag.Args()
 	}
- 
+
+	var wg sync.WaitGroup
+	wg.Add(len(targets))
 	for _, arg := range targets {
-		// Find and fetch favicon:
-		f := internal.New()
-		favicons, err := f.FetchFavicons(arg)
-		if err != nil {
-			log.Fatalf("error: %v", err)
-		}
-
-		if len(favicons) == 0 {
-			log.Fatalf("error: no favicons for %s found", arg)
-			break
-		}
-
-		for idxIco, icon := range favicons {
-			u, err := url.Parse(
-				arg,
-			)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Save to disk:
-			curtime := time.Now()
-			fname := fmt.Sprintf("%s-%s-%d.ico",
-				fmt.Sprintf(
-					"%d%02d%02d%02d%02d%02d",
-					curtime.Year(), curtime.Month(), curtime.Day(),
-					curtime.Hour(), curtime.Minute(), curtime.Second(),
-				),
-				u.Host,
-				idxIco)
-			out, err := os.Create(fname)
+		go func(arg string) {
+			defer wg.Done()
+			log.Printf("Starting goroutine for %v", arg)
+			// Find and fetch favicon:
+			f := internal.New()
+			favicons, err := f.FetchFavicons(arg)
 			if err != nil {
 				log.Fatalf("error: %v", err)
 			}
-			defer out.Close()
 
-			_, err = out.Write(icon)
-			if err != nil {
-				log.Fatalf("error: %v", err)
+			if len(favicons) == 0 {
+				log.Fatalf("error: no favicons for %s found", arg)
+				return
 			}
-		}
+
+			for idxIco, icon := range favicons {
+				u, err := url.Parse(
+					arg,
+				)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// Save to disk:
+				curtime := time.Now()
+				fname := fmt.Sprintf("%s-%s-%d.ico",
+					fmt.Sprintf(
+						"%d%02d%02d%02d%02d%02d",
+						curtime.Year(), curtime.Month(), curtime.Day(),
+						curtime.Hour(), curtime.Minute(), curtime.Second(),
+					),
+					u.Host,
+					idxIco)
+				out, err := os.Create(fname)
+				if err != nil {
+					log.Fatalf("error: %v", err)
+				}
+				defer out.Close()
+
+				_, err = out.Write(icon)
+				if err != nil {
+					log.Fatalf("error: %v", err)
+				}
+			}
+		}(arg)
 	}
-
+	wg.Wait()
 }
